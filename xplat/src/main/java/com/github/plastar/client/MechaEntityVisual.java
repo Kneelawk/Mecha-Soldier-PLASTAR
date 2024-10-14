f7c364a42af408f8feeb482fdf3810b700b37945
MattiDragon <matias.koivikko@gmail.com>
2024-10-14 17:29:38 +0300

    Part wise mecha rendering and basic animations

--- xplat/src/main/java/com/github/plastar/client/MechaEntityVisual.java
+++ xplat/src/main/java/com/github/plastar/client/MechaEntityVisual.java
@@ -1,79 +1,129 @@
 package com.github.plastar.client;
 
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.EnumMap;
+import java.util.Map;
 
 import com.github.plastar.Constants;
-import com.github.plastar.Log;
 import com.github.plastar.client.model.MechaModelManager;
+import com.github.plastar.data.Mecha;
 import com.github.plastar.data.MechaSection;
 import com.github.plastar.entity.MechaEntity;
 
+import dev.engine_room.flywheel.api.visual.DynamicVisual;
+import dev.engine_room.flywheel.api.visual.TickableVisual;
 import dev.engine_room.flywheel.api.visualization.VisualizationContext;
 import dev.engine_room.flywheel.lib.instance.InstanceTypes;
 import dev.engine_room.flywheel.lib.instance.TransformedInstance;
-import dev.engine_room.flywheel.lib.model.SimpleModel;
 import dev.engine_room.flywheel.lib.visual.ComponentEntityVisual;
+import dev.engine_room.flywheel.lib.visual.SimpleTickableVisual;
 import dev.engine_room.flywheel.lib.visual.component.FireComponent;
 import dev.engine_room.flywheel.lib.visual.component.HitboxComponent;
 import dev.engine_room.flywheel.lib.visual.component.ShadowComponent;
 
 import net.minecraft.client.resources.model.Material;
-import net.minecraft.resources.ResourceLocation;
 
-public class MechaEntityVisual extends ComponentEntityVisual<MechaEntity> {
-    private final TransformedInstance instance;
+import net.minecraft.util.Mth;
+
+import org.joml.Matrix4fStack;
+
+public class MechaEntityVisual extends ComponentEntityVisual<MechaEntity> implements SimpleTickableVisual {
+    private final Matrix4fStack matrixStack = new Matrix4fStack(2);
+    private final Map<MechaSection, TransformedInstance> instances = new EnumMap<>(MechaSection.class);
+    private Mecha mecha;
 
     public MechaEntityVisual(VisualizationContext ctx, MechaEntity entity, float partialTick) {
         super(ctx, entity, partialTick);
 
-        var model = MechaModelManager.INSTANCE.getModel(Constants.rl("test_model"));
-        if (model == null) {
-            Log.LOG.warn("Failed to load model for mecha");
-            instance = instancerProvider().instancer(InstanceTypes.TRANSFORMED, new SimpleModel(List.of())).createInstance();
-        } else {
-            var torso = entity.getMecha().parts().get(MechaSection.TORSO);
-            var texture = torso != null ? Constants.rl("mecha/test_part/" + torso.pattern() + "_" + torso.palette()) : pickTexture(entity.getUUID());
-            var material = new Material(Constants.ATLAS_ID, texture);
-            instance = instancerProvider().instancer(InstanceTypes.TRANSFORMED, model.getModel(material)).createInstance();
-        }
+        mecha = entity.getMecha();
+        rebuildInstances();
 
         addComponent(new ShadowComponent(visualizationContext, entity).radius(0.7f));
         addComponent(new FireComponent(visualizationContext, entity));
         addComponent(new HitboxComponent(visualizationContext, entity));
-        
-        instance.setChanged();
     }
-    
-    private ResourceLocation pickTexture(UUID uuid) {
-        var random = new Random(uuid.hashCode());
-        var pattern = switch (random.nextInt(3)) {
-            case 0 -> "checker";
-            case 1 -> "striped";
-            case 2 -> "core";
-            default -> throw new IllegalStateException();
-        };
-        var palette = switch (random.nextInt(3)) {
-            case 0 -> "a";
-            case 1 -> "b";
-            case 2 -> "c";
-            default -> throw new IllegalStateException();
-        };
-        return Constants.rl("mecha/test_part/" + pattern + "_" + palette);
+
+    private void rebuildInstances() {
+        instances.values().forEach(TransformedInstance::delete);
+        instances.clear();
+        for (var part : mecha.parts().values()) {
+            var model = MechaModelManager.INSTANCE.getModel(Constants.rl(part.section().getSerializedName()));
+            if (model == null) continue;
+            var texture = Constants.rl("mecha/test_part/" + part.pattern() + "_" + part.palette());
+            var material = new Material(Constants.ATLAS_ID, texture);
+            instances.put(part.section(), instancerProvider().instancer(InstanceTypes.TRANSFORMED, model.getModel(material)).createInstance());
+        }
     }
 
     @Override
-    public void beginFrame(Context ctx) {
+    public void tick(TickableVisual.Context context) {
+        if (mecha != entity.getMecha()) {
+            mecha = entity.getMecha();
+            rebuildInstances();
+        }
+    }
+
+    @Override
+    public void beginFrame(DynamicVisual.Context ctx) {
         super.beginFrame(ctx);
-        instance.setIdentityTransform()
-            .translate(getVisualPosition(ctx.partialTick()))
-            .light(computePackedLight(ctx.partialTick()))
-            .setChanged();
+        var partialTick = ctx.partialTick();
+        
+        matrixStack.identity();
+        matrixStack.translate(getVisualPosition(partialTick));
+        var bodyRot = 180 - Mth.rotLerp(partialTick, entity.yBodyRotO, entity.yBodyRot);
+        matrixStack.rotateY(bodyRot * Mth.DEG_TO_RAD);
+        
+        var limbSwingAmount = entity.walkAnimation.speed(partialTick);
+        var limbSwing = entity.walkAnimation.position(partialTick);
+
+        for (var entry : instances.entrySet()) {
+            var instance = entry.getValue();
+            matrixStack.pushMatrix();
+            switch (entry.getKey()) {
+                case HEAD -> {
+                    var headRot = 180 - Mth.rotLerp(partialTick, entity.yHeadRotO, entity.yHeadRot);
+                    var netHeadRot = Mth.wrapDegrees(headRot - bodyRot);
+                    matrixStack.rotateY(netHeadRot * Mth.DEG_TO_RAD);
+                    matrixStack.translate(0, 13 / 16f, 0);
+                    matrixStack.rotateX(-Mth.lerp(partialTick, entity.xRotO, entity.getXRot()) * Mth.DEG_TO_RAD);
+                    matrixStack.translate(0, -13 / 16f, 0);
+                }
+                case TORSO -> {}
+                case LEFT_ARM -> {
+                    matrixStack.translate(5 / 16f, 12 / 16f, 0);
+                    matrixStack.rotateX(Mth.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F);
+                    matrixStack.translate(-5 / 16f, -12 / 16f, 0);
+                }
+                case RIGHT_ARM -> {
+                    matrixStack.translate(-5 / 16f, 12 / 16f, 0);
+                    matrixStack.rotateX(Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 2.0F * limbSwingAmount * 0.5F);
+                    matrixStack.translate(5 / 16f, -12 / 16f, 0);
+                }
+                case LEFT_LEG -> {
+                    matrixStack.translate(2 / 16f, 3 / 16f, 0);
+                    matrixStack.rotateX(Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount);
+                    matrixStack.translate(-2 / 16f, -3 / 16f, 0);
+                }
+                case RIGHT_LEG -> {
+                    matrixStack.translate(-2 / 16f, 3 / 16f, 0);
+                    matrixStack.rotateX(Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount);
+                    matrixStack.translate(2 / 16f, -3 / 16f, 0);
+                }
+                case BACKPACK -> {}
+                case WEAPON -> {}
+                case SHIELD -> {}
+            }
+            
+            instance.setIdentityTransform()
+                .setTransform(matrixStack)
+                .light(computePackedLight(partialTick))
+                .setChanged();
+            matrixStack.popMatrix();
+        }
     }
 
     @Override
     protected void _delete() {
-        instance.delete();
+        super._delete();
+        instances.values().forEach(TransformedInstance::delete);
     }
 }
--- xplat/src/main/java/com/github/plastar/data/Mecha.java
+++ xplat/src/main/java/com/github/plastar/data/Mecha.java
@@ -1,13 +1,17 @@
 package com.github.plastar.data;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.BiConsumer;
+
+import com.google.common.collect.ImmutableMap;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import net.minecraft.Util;
 import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -37,7 +41,12 @@ public record Mecha(Component name, Map<MechaSection, MechaPart> parts) {
         ByteBufCodecs.map(HashMap::new, MechaSection.STREAM_CODEC, MechaPart.STREAM_CODEC), Mecha::parts,
         Mecha::new);
 
-    public static final Mecha DEFAULT = new Mecha(Component.empty(), Collections.emptyMap());
+    public static final Mecha DEFAULT = new Mecha(Component.empty(), Util.make(ImmutableMap.<MechaSection, MechaPart>builder(), builder -> {
+        var sections = Arrays.asList(MechaSection.HEAD, MechaSection.TORSO, MechaSection.LEFT_ARM, MechaSection.RIGHT_ARM, MechaSection.LEFT_LEG, MechaSection.RIGHT_LEG);
+        for (var section : sections) {
+            builder.put(section, new MechaPart(section, new PartMaterial(Optional.empty()), "striped", "a"));
+        }
+    }).build());
 
     public void forEachAttributeModifier(ReloadableServerRegistries.Holder registries, BiConsumer<Holder<Attribute>, AttributeModifier> consumer) {
         parts.forEach((section, part) -> part.material().forEachAttributeModifier(registries, section, consumer));
