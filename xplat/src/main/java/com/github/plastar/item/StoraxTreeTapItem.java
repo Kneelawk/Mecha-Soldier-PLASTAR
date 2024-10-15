9b973d4bb093a012fe9e58e388758580efce6e09
June <khazoda@gmail.com>
2024-10-15 20:32:03 +0100

    tree tap item skeleton

---
+++ xplat/src/main/java/com/github/plastar/item/StoraxTreeTapItem.java
@@ -0,0 +1,9 @@
+package com.github.plastar.item;
+
+import net.minecraft.world.item.Item;
+
+public class StoraxTreeTapItem extends Item {
+    public StoraxTreeTapItem(Properties properties) {
+        super(properties);
+    }
+}
