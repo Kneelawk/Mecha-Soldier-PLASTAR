dc6c9c5afd027d20ff8b74ccf1316c270c00a1b2
June <khazoda@gmail.com>
2024-10-16 14:15:13 +0100

    package annotations for block/ and world/

---
+++ xplat/src/main/java/com/github/plastar/world/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package com.github.plastar.world;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
