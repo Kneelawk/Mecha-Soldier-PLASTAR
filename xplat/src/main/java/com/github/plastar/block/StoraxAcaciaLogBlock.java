cee7e2a0b678096830c31121b53e4e8a5e53c346
June <khazoda@gmail.com>
2024-10-21 15:35:57 +0100

    ticking fix/balance change for storax acacia

--- xplat/src/main/java/com/github/plastar/block/StoraxAcaciaLogBlock.java
+++ xplat/src/main/java/com/github/plastar/block/StoraxAcaciaLogBlock.java
@@ -1,33 +1,24 @@
 package com.github.plastar.block;
 
-import com.mojang.serialization.Codec;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.ChangeOverTimeBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 
-import java.util.Optional;
-
 /**
  * @Summary Acacia Log filled with Storax (tree sap) used to create Styrol & Polystyrene
- *
  * @Loot_table This block's loot table drops a normal acacia log and 1-3 styrol by default.
  * If silk touched, this storax-filled acacia block will drop, but no styrol
- *
  * @Interaction When right-clicked with a tree tap, this block drops 3-6 styrol and changes SAPPY state to false.
  * It becomes SAPPY again over time.
  **/
@@ -40,9 +31,17 @@ public class StoraxAcaciaLogBlock extends RotatedPillarBlock {
         super(PROPERTIES);
     }
 
+    /* Only ticks if not sappy */
+    @Override
+    protected boolean isRandomlyTicking(BlockState state) {
+        return !state.getValue(SAPPY);
+    }
+
+    /* Regenerate to sappy state over time */
     @Override
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
         if (!level.isLoaded(pos)) return;
+        if (random.nextIntBetweenInclusive(1,4) != 1) return;
         level.setBlockAndUpdate(pos, PBlocks.STORAX_ACACIA_LOG_BLOCK.get().defaultBlockState());
     }
 
