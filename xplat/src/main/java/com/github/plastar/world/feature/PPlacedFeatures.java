03693865b30091f7fb2fd40c3a8de37371cb0d11
snakefangox <14956300+snakefangox@users.noreply.github.com>
2024-11-01 15:42:52 +1300

    Fix tree generation

--- xplat/src/main/java/com/github/plastar/world/feature/PPlacedFeatures.java
+++ xplat/src/main/java/com/github/plastar/world/feature/PPlacedFeatures.java
@@ -2,25 +2,55 @@ package com.github.plastar.world.feature;
 
 import com.github.plastar.Constants;
 
+import com.google.common.collect.ImmutableList;
+
 import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.util.valueproviders.WeightedListInt;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.BlockPredicateFilter;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.SurfaceWaterDepthFilter;
 
 import java.util.List;
 
 public class PPlacedFeatures {
-    public static final ResourceKey<PlacedFeature> STORAX_ACACIA = ResourceKey.create(Registries.PLACED_FEATURE, Constants.rl("storax_acacia"));
+    public static final ResourceKey<PlacedFeature> STORAX_ACACIA =
+        ResourceKey.create(Registries.PLACED_FEATURE, Constants.rl("storax_acacia"));
+    public static final ResourceKey<PlacedFeature> STORAX_TREES =
+        ResourceKey.create(Registries.PLACED_FEATURE, Constants.rl("storax_trees"));
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         context.register(STORAX_ACACIA, new PlacedFeature(
             context.lookup(Registries.CONFIGURED_FEATURE).getOrThrow(PConfiguredFeatures.STORAX_ACACIA),
             List.of(
-                BlockPredicateFilter.forPredicate(BlockPredicate.wouldSurvive(Blocks.ACACIA_SAPLING.defaultBlockState(), Vec3i.ZERO))
+                BlockPredicateFilter.forPredicate(
+                    BlockPredicate.wouldSurvive(Blocks.ACACIA_SAPLING.defaultBlockState(), Vec3i.ZERO))
             )));
+
+        var tree_density = SimpleWeightedRandomList.<IntProvider>builder()
+            .add(ConstantInt.of(0), 4)
+            .add(ConstantInt.of(1), 1)
+            .build();
+
+        context.register(STORAX_TREES, new PlacedFeature(
+            context.lookup(Registries.CONFIGURED_FEATURE).getOrThrow(PConfiguredFeatures.STORAX_ACACIA),
+            ImmutableList.<PlacementModifier>builder()
+                .add(CountPlacement.of(new WeightedListInt(tree_density)))
+                .add(InSquarePlacement.spread())
+                .add(SurfaceWaterDepthFilter.forMaxDepth(0))
+                .add(PlacementUtils.HEIGHTMAP_OCEAN_FLOOR)
+                .add(BiomeFilter.biome()).build()));
     }
 }
--- xplat/src/main/java/com/github/plastar/world/feature/StoraxLogTreeDecorator.java
+++ xplat/src/main/java/com/github/plastar/world/feature/StoraxLogTreeDecorator.java
@@ -3,13 +3,16 @@ package com.github.plastar.world.feature;
 import com.github.plastar.Constants;
 import com.github.plastar.block.PBlocks;
 import com.github.plastar.platform.Platform;
+
 import com.mojang.serialization.MapCodec;
+
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecorator;
 import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
 
 public class StoraxLogTreeDecorator extends TreeDecorator {
     public static final MapCodec<StoraxLogTreeDecorator> CODEC = MapCodec.unit(StoraxLogTreeDecorator::new);
-    public static final TreeDecoratorType<StoraxLogTreeDecorator> TYPE = Platform.INSTANCE.treeDecoratorType(Constants.rl("storax_log"), CODEC);
+    public static final TreeDecoratorType<StoraxLogTreeDecorator> TYPE =
+        Platform.INSTANCE.treeDecoratorType(Constants.rl("storax_log"), CODEC);
 
 
     @Override
@@ -21,7 +24,8 @@ public class StoraxLogTreeDecorator extends TreeDecorator {
     public void place(Context context) {
         int count = context.random().nextFloat() < 0.4 ? 2 : 1;
         for (int i = 0; i < count; ++i) {
-            context.setBlock(context.logs().get(context.random().nextInt(context.logs().size())), PBlocks.STORAX_ACACIA_LOG.get().defaultBlockState());
+            context.setBlock(context.logs().get(context.random().nextInt(context.logs().size())),
+                PBlocks.STORAX_ACACIA_LOG.get().defaultBlockState());
         }
     }
 }
