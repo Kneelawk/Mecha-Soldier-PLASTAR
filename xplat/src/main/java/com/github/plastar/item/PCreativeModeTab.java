12e1e2f03476923eedd4246a4a5c6c8582296ce5
MattiDragon <matias.koivikko@gmail.com>
2024-11-02 13:04:35 +0200

    Properly set nbt of mecha stack in inventory

--- xplat/src/main/java/com/github/plastar/item/PCreativeModeTab.java
+++ xplat/src/main/java/com/github/plastar/item/PCreativeModeTab.java
@@ -4,14 +4,19 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.github.plastar.PLASTARMod;
+import com.github.plastar.data.Mecha;
 import com.github.plastar.data.MechaPart;
 import com.github.plastar.data.PRegistries;
 import com.github.plastar.data.Palettes;
 import com.github.plastar.registry.RegistryUtil;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.component.CustomData;
 
 public class PCreativeModeTab {
     public static final Supplier<CreativeModeTab> MOD_TAB = PLASTARMod.REGISTRARS.get(Registries.CREATIVE_MODE_TAB)
@@ -25,7 +30,13 @@ public class PCreativeModeTab {
                 output.accept(PItems.POLYSTYRENE.get());
                 output.accept(PItems.NIPPERS.get());
                 output.accept(PItems.MECHA_ASSEMBLER.get());
-                output.accept(PItems.MECHA.get());
+                
+                var mechaStack = PItems.MECHA.get().getDefaultInstance();
+                var mecha = Mecha.getDefault(parameters.holders());
+                var nbt = Mecha.CODEC.fieldOf("mecha").codec()
+                    .encodeStart(parameters.holders().createSerializationContext(NbtOps.INSTANCE), mecha).getOrThrow();
+                mechaStack.set(DataComponents.BUCKET_ENTITY_DATA, CustomData.of((CompoundTag) nbt));
+                output.accept(mechaStack);
 
                 var partRegistry = parameters.holders().lookupOrThrow(PRegistries.PART);
                 var palette = RegistryUtil.getPreferred(Palettes.UNPAINTED, parameters.holders().lookupOrThrow(PRegistries.PALETTE));
