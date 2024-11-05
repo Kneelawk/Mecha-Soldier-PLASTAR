507bac37d1a4367e62cb51ef69db9c7e8979148f
MattiDragon <matias.koivikko@gmail.com>
2024-11-05 16:27:10 +0200

    Manually specify JiJ on fabric

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -16,18 +16,18 @@ kpublish {
     createPublication()
 }
 
-configurations {
-    register("includeTransitive") {
-        extendsFrom(project(":xplat").configurations["includeTransitive"])
-    }
-}
-
 dependencies {
     val mod_menu_version: String by project
     modLocalRuntime("com.terraformersmc:modmenu:$mod_menu_version") {
         exclude(group = "net.fabricmc")
         exclude(group = "net.fabricmc.fabric-api")
     }
+    include("de.javagl:jgltf-model:2.0.4")
+    include("de.javagl:jgltf-impl-v1:2.0.4")
+    include("de.javagl:jgltf-impl-v2:2.0.4")
+    include("com.fasterxml.jackson.core:jackson-core:2.13.1")
+    include("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    include("com.fasterxml.jackson.core:jackson-annotations:2.13.4")
 }
 
 loom {
@@ -38,10 +38,6 @@ fabricApi.configureDataGeneration {
     outputDirectory = project(":xplat").file("src/main/generated")
     addToResources = false
     createSourceSet = true
-}
-
-tasks.named<NestableJarGenerationTask>("processIncludeJars") {
-    from(configurations.named("includeTransitive").get())
 }
 
 // Hack to disable junit tests (which we don't use) in order to
