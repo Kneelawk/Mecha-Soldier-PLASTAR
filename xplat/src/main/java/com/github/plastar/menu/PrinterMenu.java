a1c0ddc97a4f38bfa8d2fdbc1b03143e244c7f5b
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-11-28 02:36:12 +1100

    Fix recipes not updating when taking the result item.

--- xplat/src/main/java/com/github/plastar/menu/PrinterMenu.java
+++ xplat/src/main/java/com/github/plastar/menu/PrinterMenu.java
@@ -95,6 +95,7 @@ public class PrinterMenu extends AbstractContainerMenu {
                     }
 
                 });
+                slotsChanged(container);
                 super.onTake(player, stack);
             }
 
